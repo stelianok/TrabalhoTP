8b3d6441754db3eea8f83fcd7a275eb50f453a12
Kauã Steliano <stelianok@gmail.com>
2024-06-16 18:46:14 -0300

    Created Window that shows all registered categories

--- src/ListAllCategoriesWindow.java
+++ src/ListAllCategoriesWindow.java
@@ -1,11 +1,37 @@
 import java.awt.*;
 import javax.swing.*;
 
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
 public class ListAllCategoriesWindow extends JPanel {
+
+  JLabel title = new JLabel("Lista de todas as categorias cadastradas");
+
   ListAllCategoriesWindow() {
-    setSize(800, 800);
     setLayout(new BorderLayout());
 
-    setVisible(true);
+    title.setHorizontalAlignment(SwingConstants.CENTER);
+    add(title, BorderLayout.NORTH);
+
+    DefaultTableModel model = new DefaultTableModel();
+
+    model.addColumn("ID");
+    model.addColumn("Nome");
+
+    CategoriesRepository categoriesRepository = new CategoriesRepository();
+
+    List<CategoryObject> categories = categoriesRepository.getAllCategories();
+
+    for (CategoryObject category : categories) {
+      model.addRow(new Object[] {
+          category.id, category.name
+      });
+    }
+
+    JTable table = new JTable(model);
+    JScrollPane scrollPane = new JScrollPane(table);
+
+    add(scrollPane, BorderLayout.CENTER);
   }
 }
