df35aa97b73ca4be97db98d35a082fc603f18d5e
Kauã Steliano <stelianok@gmail.com>
2024-06-16 18:46:29 -0300

    Created Window that shows all registered suppliers

--- src/ListAllSuppliersWindow.java
+++ src/ListAllSuppliersWindow.java
@@ -1,11 +1,41 @@
 import java.awt.*;
 import javax.swing.*;
 
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
 public class ListAllSuppliersWindow extends JPanel {
+
+  JLabel title = new JLabel("Lista de todos os fornecedores");
+
   ListAllSuppliersWindow() {
-    setSize(800, 800);
     setLayout(new BorderLayout());
 
-    setVisible(true);
+    title.setHorizontalAlignment(SwingConstants.CENTER);
+    add(title, BorderLayout.NORTH);
+
+    DefaultTableModel model = new DefaultTableModel();
+
+    model.addColumn("ID");
+    model.addColumn("Nome");
+    model.addColumn("Endereço");
+    model.addColumn("Contato");
+    model.addColumn("Email");
+
+    SuppliersRepository suppliersRepository = new SuppliersRepository();
+
+    List<SupplierObject> suppliers = suppliersRepository.getAllSuppliers();
+
+    for (SupplierObject supplier : suppliers) {
+      model.addRow(new Object[] {
+          supplier.id, supplier.name, supplier.address, supplier.address, supplier.email
+      });
+    }
+
+    JTable table = new JTable(model);
+    JScrollPane scrollPane = new JScrollPane(table);
+
+    add(scrollPane, BorderLayout.CENTER);
+
   }
 }
